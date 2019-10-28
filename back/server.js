518ca9d83550f13d28d00191dc315d3a6356c3f1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-28 11:55:42 +0100

    server static ok

--- back/server.js
+++ back/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const serveIndex = require('serve-index');
 
 const app = express();
-
 const port = 3000;
 
 app.use(express.static('.'));
+app.use(serveIndex('.', { icons: true }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
