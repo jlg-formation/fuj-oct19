b28e4bffc31a3d9d174f2ac4cd7b9b314b87c943
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-28 12:01:51 +0100

    add web service get

--- back/server.js
+++ back/server.js
@@ -4,6 +4,10 @@ const serveIndex = require('serve-index');
 const app = express();
 const port = 3000;
 
+app.get('/ws/reference', (req, res, next) => {
+    res.json({ toto: 345, titi: "pelle" });
+});
+
 app.use(express.static('.'));
 app.use(serveIndex('.', { icons: true }));
 
