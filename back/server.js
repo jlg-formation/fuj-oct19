37e55a984e924eb2107c8e4443b7d0c5a2cd45cb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-28 11:17:43 +0100

    serve file

--- back/server.js
+++ back/server.js
@@ -4,9 +4,7 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.static('.'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
