5760dc5abdf7dd44758ccb2f9f6cf57275819831
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-29 16:10:20 +0100

    back office ok with simple file

--- back/server.js
+++ back/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const serveIndex = require('serve-index');
 const cors = require('cors');
+const fs = require('fs');
 
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
+
+const references = [];
 
 app.get('/ws/reference', (req, res, next) => {
     res.json({ toto: 345, titi: "pelle" });
+});
+
+app.post('/ws/reference', (req, res, next) => {
+    references.push(req.body);
+    fs.writeFileSync('references.json', JSON.stringify(references));
+    res.status(204).end();
 });
 
 app.use(express.static('.'));
