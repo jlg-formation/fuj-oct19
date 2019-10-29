eda8bb9d28e90b64be113fb6d05d63e09a1d7ed6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-29 11:49:07 +0100

    added form reactive

--- front/src/app/reference-create/create/create.component.ts
+++ front/src/app/reference-create/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
+
+  f = new FormGroup({
+    label: new FormControl('', Validators.required),
+    category: new FormControl('', Validators.required),
+    quantity: new FormControl('', Validators.required),
+    price: new FormControl(''),
+  });
 
   constructor() { }
 
