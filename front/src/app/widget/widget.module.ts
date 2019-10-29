e2f1c55c00d01fd0657909565c161bd972199fd3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-29 15:41:53 +0100

    format

--- front/src/app/widget/widget.module.ts
+++ front/src/app/widget/widget.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AutofocusDirective } from './autofocus.directive';
 
-
-
 @NgModule({
   declarations: [AutofocusDirective],
   imports: [
