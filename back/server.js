d928d78c58902690dad7d8a4b707c6ec506deb09
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-29 15:51:37 +0100

    avoid cors

--- back/server.js
+++ back/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const serveIndex = require('serve-index');
+const cors = require('cors');
+
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get('/ws/reference', (req, res, next) => {
     res.json({ toto: 345, titi: "pelle" });
