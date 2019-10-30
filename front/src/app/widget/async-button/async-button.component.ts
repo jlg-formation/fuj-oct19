ce038408a91d6d50063911cc822f9567866bafa9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-30 17:29:07 +0100

    btn submit earlier

--- front/src/app/widget/async-button/async-button.component.ts
+++ front/src/app/widget/async-button/async-button.component.ts
@@ -30,16 +30,21 @@ export class AsyncButtonComponent implements OnInit, AfterContentInit, AfterView
     const btn = (this.button.nativeElement as HTMLElement);
     btn.addEventListener('click', evt => {
       console.log('evt: ', evt);
+
+      const submitBtn = this.submitButton.nativeElement as HTMLElement;
+      submitBtn.click();
+
+
       const { width, height } = btn.getBoundingClientRect();
       this.state = 'progress';
       console.log('btn.style.height: ', width, height);
+
       timer(20).subscribe(x => {
         const buttonTraceDiv = this.buttonTrace.nativeElement as HTMLElement;
         buttonTraceDiv.style.height = height + 'px';
         buttonTraceDiv.style.width = width + 'px';
         buttonTraceDiv.classList.add('start');
-        const submitBtn = this.submitButton.nativeElement as HTMLElement;
-        submitBtn.click();
+
       });
 
     });
