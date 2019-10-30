904997ed20fb48bacdc538a6c1530e2f3e3ec459
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-10-30 22:01:05 +0100

    delivered route created

--- front/src/app/sell/sell-routing.module.ts
+++ front/src/app/sell/sell-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
+import { DeliveredComponent } from './delivered/delivered.component';
 
 
 const routes: Routes = [
   { path: 'references', component: ListComponent },
   { path: 'references/:label', component: DetailComponent },
+  { path: 'delivered', component: DeliveredComponent },
 ];
 
 @NgModule({
--- front/src/app/sell/sell.module.ts
+++ front/src/app/sell/sell.module.ts
@@ -6,10 +6,11 @@ import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { WidgetModule } from '../widget/widget.module';
+import { DeliveredComponent } from './delivered/delivered.component';
 
 
 @NgModule({
-  declarations: [ListComponent, DetailComponent],
+  declarations: [ListComponent, DetailComponent, DeliveredComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
